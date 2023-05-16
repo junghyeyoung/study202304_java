b345bd9916bccc99030bc24dd7fd956da08f2bf8
JHY <rentaru2@naver.com>
2023-05-16 11:14:26 +0900

    Step17에 update, delete 메소드를 추가 작성함

--- Step17_JDBC/src/test/dao/MemberDao.java
+++ Step17_JDBC/src/test/dao/MemberDao.java
@@ -13,7 +13,69 @@ import test.util.DBConnect;
  *  
  */
 public class MemberDao {
-	
+	//회원 한명의 정보를 삭제하는 메소드
+	public boolean delete(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		int rowCount = 0;
+		try {
+			conn = new DBConnect().getConn();
+			String sql = "DELETE FROM member"
+					+ " WHERE num=? ";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rowCount = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+
+		if (rowCount > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//회원 한명의 정보를 수정하고 해당 작업의 성공여부를 리턴해주는 메소드
+	public boolean update(MemberDto dto) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		
+		int rowCount=0;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="UPDATE member"
+					+ " SET name=?, addr=? "
+					+ " WHERE num=? ";
+		
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getName());
+			pstmt.setString(2, dto.getAddr());
+			pstmt.setInt(3, dto.getNum());
+			rowCount=pstmt.executeUpdate(); 
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		
+		if(rowCount > 0) {
+			return true;
+		}else { 
+			return false;
+		}
+	}
 	// 회원 한명의 정보를 저장하고 해당 작업의 성공여부를 리턴해주는 메소드
 	public boolean insert(MemberDto dto) {
 		//필요한 객체를 담을 지역변수를 미리 만들기
